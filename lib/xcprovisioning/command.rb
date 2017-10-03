15879baa6c455f77acf4066c3352b744ea8c9c2b
y-hryk <dev.hy630823@gmail.com>
2017-10-03 19:49:35 +0900

    refactoring

--- lib/xcprovisioning/command.rb
+++ lib/xcprovisioning/command.rb
@@ -15,9 +15,9 @@ module Xcprovisioning
       # current provisioning profile
       current_rows = []
       provisiong.each { |profile|  
-        current_rows << [ profile.name, profile.application_identifier, profile.team_name, profile.creation_date]
+        current_rows << [ profile.name, profile.application_identifier, profile.team_name, profile.expiration_date]
       }
-      current_table = Terminal::Table.new :title => "Provisioning Profile", :headings => ['name', 'application_identifier', 'team_name','creation_date'], :rows => current_rows
+      current_table = Terminal::Table.new :title => "Provisioning Profile", :headings => ['name', 'application_identifier', 'team_name','expiration_date'], :rows => current_rows
       puts current_table
 
     end
@@ -27,31 +27,13 @@ module Xcprovisioning
 
       # fetch provisioning profile list
       mobileDevice = MobileDevice.new()
-      provisiong = mobileDevice.provisionings()
-
-      appId_group = provisiong.group_by do |profile|
-        profile.application_identifier
-      end
-
-      remove_profiles = []
-      appId_group.each_value {|val|
-
-        if val.count > 1
-          recent_profile = val.max_by {|value| value.creation_date}
-          val.delete(recent_profile)
-          val.each { |profile|  
-            if profile.name == recent_profile.name
-              remove_profiles << profile
-            end
-          }
-        end
-      }
+      remove_profiles = mobileDevice.deletion_targets()
 
       remove_rows = []
       remove_profiles.each { |profile|  
-        remove_rows << [ profile.name, profile.application_identifier, profile.team_name, profile.file_name, profile.creation_date]
+        remove_rows << [ profile.name, profile.application_identifier, profile.team_name, profile.expiration_date]
       }
-      remove_table = Terminal::Table.new :title => "Duplicate Provisioning Profile", :headings => ['name', 'application_identifier', 'team_name','file_name','creation_date'], :rows => remove_rows
+      remove_table = Terminal::Table.new :title => "Duplicate Provisioning Profile", :headings => ['name', 'application_identifier', 'team_name','expiration_date'], :rows => remove_rows
 
       if !remove_rows.empty?
         puts remove_table
